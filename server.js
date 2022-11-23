e25933ea7190821559169c76bf110dde4a098bec
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2022-11-23 10:58:48 +0100

    Set up live server and create a p5 project inside

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("running")
+console.log("running");
+
+let express = require("express"); //load the express library
+
+let app = express(); //activte express and put it in a variable called app
+
+let port = 3000; //porta del server per comunicare
+
+let server = app.listen(port) //listen = wait for imput from (port)
+
+app.use(express.static("public")); //magic line che collega server e client
+
+console.log('running server on http://localhost:' + port)
