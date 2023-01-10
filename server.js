1c61b63638000ff6db4f9266567ad57c1134e784
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2023-01-10 10:43:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express"); //load the express library
 
 let app = express(); //activte express and put it in a variable called app
 
-let port = 3000; //porta del server per comunicare
+let port = process.env.PORT || 3000; //porta del server per comunicare
 
 let server = app.listen(port) //listen = wait for imput from (port)
 
